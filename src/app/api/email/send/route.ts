52d9c2e0f2fd423159ce2018d62c8deeccf45c09
donattodev <donattodev@gmail.com>
2024-03-14 15:33:22 -0300

    email send

--- src/app/api/email/send/route.ts
+++ src/app/api/email/send/route.ts
@@ -1,16 +1,16 @@
 import { Resend } from 'resend'
 import { NextResponse } from 'next/server'
 
-export async function GET() {
+export async function POST() {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY)
-    await resend.emails.send({
-      from: 'contato@donattodev.com',
-      to: 'felippedonatto0@gmail.com',
+    const { data } = await resend.emails.send({
+      from: 'donattodev@gmail.com',
+      to: 'donattodev@gmail.com',
       subject: 'Testando envio de e-mails',
       html: '<p>è otimo ver tudo funcionando!</p>',
     })
-    return NextResponse.json({ message: 'Email Send' })
+    return NextResponse.json({ message: 'Email Send', data })
   } catch (err) {
     return NextResponse.json({ err })
   }
