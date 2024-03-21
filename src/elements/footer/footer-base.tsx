1c6959a3f9d97d26624fcc9b68a71a8334d80c5e
donattodev <donattodev@gmail.com>
2024-03-21 13:20:16 -0300

    Update 02

--- src/elements/footer/footer-base.tsx
+++ src/elements/footer/footer-base.tsx
@@ -2,8 +2,11 @@ export function FooterBase() {
   return (
     <footer className="flex flex-col items-center justify-center bg-zinc-900 p-6">
       <p className="text-sm font-bold text-zinc-400 max-sm:text-center">
-        Todos os direitos reservados. Criado por{' '}
-        <span className="text-primary">Felippe Donatto</span>
+        Copyright &#169; 2024 -{' '}
+        <span className="text-sm font-bold uppercase text-primary">
+          Donatto dev
+        </span>{' '}
+        - Todos os direitos reservados.
       </p>
     </footer>
   )
