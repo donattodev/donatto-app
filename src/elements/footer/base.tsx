c4b995906362cf18693e1dcc7d0413adeb745d20
Donatto Dev <130395087+donattodev@users.noreply.github.com>
2024-05-03 19:02:49 -0300

    Update

--- src/elements/footer/base.tsx
+++ src/elements/footer/base.tsx
@@ -4,6 +4,7 @@ import { BehanceIcon } from '@/components/icons/behance'
 import { GithubIcon } from '@/components/icons/github'
 import { InstagramIcon } from '@/components/icons/instagram'
 import { YoutubIcon } from '@/components/icons/youtube-icon'
+import { LinkedinIcon } from '@/components/icons/linkedin'
 
 export function Base() {
   return (
@@ -11,13 +12,21 @@ export function Base() {
       <div>
         <ul className="flex items-center gap-3">
           <li className="animate-down rounded bg-zinc-950 p-2 transition-all hover:bg-primary">
-            <Link href="https://github.com/felippedonatto" target="_blank">
+            <Link href="https://github.com/donattodev" target="_blank">
               <GithubIcon className="icon w-5" />
             </Link>
           </li>
           <li className="animate-down rounded bg-zinc-950 p-2 transition-all hover:bg-primary">
             <Link href="https://www.behance.net/felippedon8296" target="_blank">
               <BehanceIcon className="icon w-5" />
+            </Link>
+          </li>
+          <li className="animate-down rounded bg-zinc-950 p-2 transition-all hover:bg-primary">
+            <Link
+              href="https://www.linkedin.com/in/felippe-donatto-oliveira-7212a4121/"
+              target="_blank"
+            >
+              <LinkedinIcon className="icon w-5" />
             </Link>
           </li>
           <li className="animate-down rounded bg-zinc-950 p-2 transition-all hover:bg-primary">
