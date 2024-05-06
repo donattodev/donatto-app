4320c1230a0d5e850d4d34fcbb8c3beb62bfa59c
Donatto Dev <130395087+donattodev@users.noreply.github.com>
2024-05-06 10:31:52 -0300

    update 01

--- src/elements/footer/areas.tsx
+++ src/elements/footer/areas.tsx
@@ -4,34 +4,36 @@ export function Area() {
   return (
     <>
       <div className="col-span-1 flex flex-col gap-4">
-        <h2 className="text-sm font-bold uppercase">Links Rápidos</h2>
+        <h2 className="animate-left text-sm font-bold uppercase">
+          Links Rápidos
+        </h2>
 
         <ul className="flex flex-col gap-2 font-sans text-sm font-semibold text-zinc-400">
-          <li className="transition-all hover:text-zinc-50">
+          <li className="animate-left transition-all hover:text-zinc-50">
             <Link href="/">Home</Link>
           </li>
-          <li className="transition-all hover:text-zinc-50">
+          <li className="animate-left transition-all hover:text-zinc-50">
             <Link href="/projetos">Projetos</Link>
           </li>
-          <li className="transition-all hover:text-zinc-50">
+          <li className="animate-left transition-all hover:text-zinc-50">
             <Link href="/sobre">Sobre</Link>
           </li>
-          <li className="transition-all hover:text-zinc-50">
+          <li className="animate-left transition-all hover:text-zinc-50">
             <Link href="https://blog.donattodev.com.br" target="_blank">
               Blog
             </Link>
           </li>
-          <li className="transition-all hover:text-zinc-50">
+          <li className="animate-left transition-all hover:text-zinc-50">
             <Link href="/contato">Contato</Link>
           </li>
         </ul>
       </div>
 
       <div className="col-span-1 flex flex-col gap-4">
-        <h2 className="text-sm font-bold uppercase">LGPD</h2>
+        <h2 className="animate-left text-sm font-bold uppercase">LGPD</h2>
 
         <ul className="flex flex-col gap-2 font-sans text-sm font-semibold text-zinc-400">
-          <li className="transition-all hover:text-zinc-50">
+          <li className="animate-left transition-all hover:text-zinc-50">
             <Link href="/">Politicas e privacidade</Link>
           </li>
         </ul>
--- src/elements/footer/field.tsx
+++ src/elements/footer/field.tsx
@@ -6,7 +6,7 @@ export type FieldProps = ComponentProps<'div'> & {
 
 export function Field({ children }: FieldProps) {
   return (
-    <div className="flex flex-row items-start justify-between bg-zinc-900 px-56 py-28 max-lg:px-16 max-sm:px-6">
+    <div className="flex flex-row items-start justify-between bg-zinc-900 px-56 py-16 max-sm:flex-col max-sm:gap-6 max-sm:px-6">
       {children}
     </div>
   )
